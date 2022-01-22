60cf388b526ad40bb8f669c58f4a7afb9e134e34
dianaosauzou <61782163+dianaosauzou@users.noreply.github.com>
2022-01-22 19:10:27 +0000

    url sanitizing

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,6 +8,7 @@ import {AppService} from 'src/app/services/app-service.service';
 import {ItemService} from 'src/app/services/item-service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {UploadService} from 'src/app/services/upload-service.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profile',
@@ -21,10 +22,9 @@ export class ProfileComponent implements OnInit {
   id= sessionStorage.getItem('id');
   images: ContentInterface;
 
-  constructor(public dialog: MatDialog, private userService: UserServiceService, private app: AppService, private http: HttpClient, private itemS: ItemService, private upload: UploadService) { 
+  constructor(public dialog: MatDialog, private userService: UserServiceService, private app: AppService, private http: HttpClient, private itemS: ItemService, private upload: UploadService, public domSanitizationService: DomSanitizer,) { 
     http.get('server/api/v1/token').subscribe(data => {
       const token = data['token'];
-      // console.log(token);
       }, () => {});
   }
 
@@ -38,17 +38,13 @@ export class ProfileComponent implements OnInit {
         height: '500px',
                 data:{color: this.item.color, size: this.item.size, material: this.item.material, condition: this.item.condition, posts: this.item.formData},
       });
-      // console.log()
-
       dialogRef.afterClosed().subscribe(result => {
 
-        // console.log(result)
         this.item.color=result.color
         this.item.size=0
         this.item.condition = result.condition
         this.item.material=result.material
         this.item.formData = result.posts
-        // this.item.images.push(result.posts)
         if(result){
           this.upload.uploadFile(this.item.formData)
           .subscribe(data => 
@@ -63,33 +59,19 @@ export class ProfileComponent implements OnInit {
           }
 
       });
-
-
   }
-
-  //this display method will retrieve
-  //get by logged in user
 
   display(){
     this.userService.getPosts(this.id).subscribe(
       data=>{
-
-        //we might have to retireve item instead, have a nestsed slideshow
         this.mediaArray= new Array()
         console.log(data)
-
-
-
         for(let x in data){
-        
           this.images = new ContentInterface
           this.images.fileName=data[x].fileName;
           this.images.fileType=data[x].fileType;
-          this.images.data= 'data:image/jpeg;base64,'+ data[x].data;
-       
+          this.images.data= this.domSanitizationService.bypassSecurityTrustUrl('data:image/jpeg;base64,'+ data[x].data);
           this.mediaArray.push(this.images)
-
-
         }
      } ,
       error => { 
@@ -99,17 +81,8 @@ export class ProfileComponent implements OnInit {
 
   authenticated() { return this.app.authenticated; }
 
-
-  //now it's retrieving posts from items
-  //user.get items, retrieve the post
-
-  //
-
   ngOnInit(): void {
-    //sessionStorage works
-
-    // this.display()
-
+    this.display()
   }
 
 }
